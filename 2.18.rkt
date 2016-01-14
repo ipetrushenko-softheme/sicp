803747152f1ec2f5e0dd072f1f682e59a40f63e1
ipetrushenko-softheme <ivan.petrushenko@softheme.com>
2016-01-14 08:37:24 +0200

    Update reverse and create square-list based on it

--- 2.18.rkt
+++ 2.18.rkt
@@ -19,3 +19,15 @@
        null
        (cons(last-pair items)
             (reverse (list-without-last-pair items)))))
+
+;2 version
+
+(define (reverse2 items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons (car things) answer))))
+  (iter items `()))
+
+(reverse2 (list 1 2 3))
