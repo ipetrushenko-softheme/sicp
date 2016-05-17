c8bcadb9aab5d1960812fe332922945e16c398e6
ipetrushenko-softheme <ivan.petrushenko@softheme.com>
2016-05-17 16:42:35 +0300

    Add directory per chapter

--- 1.12.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-
-(define (packal-triangle row col)
-  (cond ((> col row) 0)
-        ((< col 0) 0)
-        ((= col 1) 1)
-        ((+ (packal-triangle (- row 1) (- col 1))
-            (packal-triangle (- row 1) col)))))
--- 2.23.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(define (foreach func items)
-  (cond ((null? items) #t)
-        (else (func (car items))
-              (foreach func (cdr items)))))
-
-(foreach (lambda(x) (newline) (display x))
-         (list 57 321 88))
